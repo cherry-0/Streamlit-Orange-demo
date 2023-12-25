fd2ed55728f39be0e85169265a2a4c796a0f442b
sieun <susanpark0@snu.ac.kr>
2023-12-25 12:52:47 +0900

    removed sudo

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #source ../miniconda3/etc/profile.d/conda.sh
-sudo apt-get update
-sudo apt-get install libgl1-mesa-glx
+apt-get install libgl1-mesa-glx
 pip install PyQt5
 pip install orange3
 python -m Orange.canvas
