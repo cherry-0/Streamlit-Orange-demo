d8b488c7c36315a088fe94dec6feb5383d4e7aab
sieun <susanpark0@snu.ac.kr>
2023-12-25 12:28:41 +0900

    minor fix

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #source ../miniconda3/etc/profile.d/conda.sh
-conda create -n orange 
-conda activate orange
-conda install -c conda-forge orange3
+pip install orange3
 python -m Orange.canvas
