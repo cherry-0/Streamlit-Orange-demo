a7bb16496e8688349d133160d056ca703d9bf83c
sieun <susanpark0@snu.ac.kr>
2023-12-25 12:25:00 +0900

    test.sh for noenv setting

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source ../miniconda3/etc/profile.d/conda.sh
+#source ../miniconda3/etc/profile.d/conda.sh
+conda create -n orange 
 conda activate orange
+conda install -c conda-forge orange3
 python -m Orange.canvas
